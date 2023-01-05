b5cbd545cefa4f22ad57543460947a3c9b2eb5b7
Cris Jr <crisjr@pm.me>
2023-01-05 11:47:12 -0300

    SQUASH trying to find out why the database doesn't always commit operations

--- models/clients.go
+++ models/clients.go
@@ -1,8 +1,38 @@
 package models
 
 import (
+    "errors"
+    "encoding/json"
     "liberdade.bsb.br/baas/api/common"
 )
+
+// Generates a new auth key
+func (context *Context) NewAuthKey(email string, isAdmin bool) (string, error)  {
+    salt := context.Config["server_salt"]
+    isAdminValue := "off"
+
+    if isAdmin {
+	isAdminValue = "on"
+    }
+    authKeyPayload := map[string]string {
+	"email": email,
+	"is_admin": isAdminValue,
+    }
+    authKeyBytes, err := json.Marshal(authKeyPayload)
+    if err != nil {
+	return "", err
+    }
+    authKeyJson := string(authKeyBytes)
+
+    authKey, err := common.Encode(authKeyJson, salt)
+
+    return authKey, err
+}
+
+// Loads an auth key into a map relating the keys "email" and "is_admin" to their respective values
+func (context *Context) ParseAuthKey(authKey string) (map[string]string, error) {
+    return nil, errors.New("Not implemented yet!")
+}
 
 // Creates a new client account
 func (context *Context) CreateClientAccount(email string, password string, isAdmin bool) (string, error) {
@@ -11,7 +41,7 @@ func (context *Context) CreateClientAccount(email string, password string, isAdm
     if isAdmin {
         isAdminToStore = "on"
     }
-    authKey, err := common.Encode(email, salt)
+    authKey, err := context.NewAuthKey(email, isAdmin)
     if err != nil {
         return "", err
     }
@@ -49,6 +79,7 @@ func (context *Context) LoginClient(email string, password string) (string, erro
     var isAdmin bool
     var authKey string
     rows.Scan(&queriedEmail, &isAdmin, &authKey)
+
     return authKey, nil
 }
 
