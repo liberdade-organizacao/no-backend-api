1d3c1a7e22cc4e402771e591c61e501ff3577332
Cris Jr <crisjr@pm.me>
2023-01-16 12:58:53 -0300

    Proxy scripting engine to run actions

--- src/br/bsb/liberdade/baas/proxies.clj
+++ src/br/bsb/liberdade/baas/proxies.clj
@@ -5,19 +5,20 @@
 
 (def scripting-engine-url (or (System/getenv "SCRIPTING_ENGINE_URL") "http://localhost:8080"))
 
-(defn run-action [client-auth-key app-auth-key action-name action-param]
-  (let [client-info (utils/decode-secret client-auth-key)
-        client-id (:client_id client-info)
-	app-info (utils/decode-secret app-auth-key)
-	app-id (:app_id app-info)
-	params {"client_id" client-id
-	        "app_id" app-id
-		"action_name" action-name
-		"action_param" action-param}
-	url (str scripting-engine-url "/actions/run")]
+(defn run-action [user-auth-key app-auth-key action-name action-param]
+  (let [user-info (utils/decode-secret user-auth-key)
+        user-id (:user_id user-info)
+        app-info (utils/decode-secret app-auth-key)
+        app-id (:app_id app-info)
+        params {"user_id" user-id
+                "app_id" app-id
+                "action_name" action-name
+                "action_param" action-param}
+        url (str scripting-engine-url "/actions/run")]
     (-> (client/post url
-                     {:body (json/write-str params)
-		      :content-type :json
-		      :accept :json})
+                     {:body (-> params json/write-str str)
+                      :content-type :json
+                      :accept :json})
+        :body
         json/read-str)))
 
