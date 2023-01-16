88932230dcbd51feabf531358deeee29ece2b449
Cris Jr <crisjr@pm.me>
2023-01-16 16:21:02 -0300

    Update scripting function names as required

--- integration/download-file.lua
+++ integration/download-file.lua
@@ -1,5 +1,5 @@
 function main(inlet)
  local params = parse_url_params(inlet)
  local filename = params["filename"]
- return download_file(filename)
+ return download_user_file(filename)
 end
--- integration/upload-file.lua
+++ integration/upload-file.lua
@@ -2,6 +2,6 @@ function main(inlet)
   local params = parse_url_params(inlet)
   local filename = params["filename"]
   local contents = params["contents"]
-  upload_file(filename, contents)
+  upload_user_file(filename, contents)
   return "ok"
 end
