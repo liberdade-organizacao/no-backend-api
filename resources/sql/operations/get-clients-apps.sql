6f4d81ad4655e6bdff93f5fb38567c8a2e6cd19b
Cris Silva Jr <crisjr@pm.me>
2023-01-23 20:06:59 -0300

    Simplify get apps query

--- resources/sql/operations/get-clients-apps.sql
+++ resources/sql/operations/get-clients-apps.sql
@@ -1,1 +1,4 @@
-SELECT app_id FROM app_memberships WHERE client_id='%{id}';
+SELECT id, name FROM apps
+WHERE id=ANY(
+    SELECT app_id FROM app_memberships WHERE app_memberships.client_id='%{client_id}'
+);
