121474cb1f731efc336308501e5e048547d3b82b
Cris Jr <crisjr@pm.me>
2023-01-26 09:55:31 -0300

    Automatically update timestamps

--- resources/sql/migrations/20230103-basic_tables.up.sql
+++ resources/sql/migrations/20230103-basic_tables.up.sql
@@ -4,8 +4,8 @@ CREATE TABLE IF NOT EXISTS clients (
     password VARCHAR(128) NOT NULL,
     is_admin BOOLEAN NOT NULL DEFAULT false,
     auth_key TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT current_timestamp NOT NULL,
-    last_updated_at TIMESTAMP DEFAULT current_timestamp NOT NULL
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp NOT NULL,
+    last_updated_at TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS apps (
