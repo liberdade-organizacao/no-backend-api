fa720bbb2fe998b0c881615082e2bc5f04bf9457
Cris Silva Jr <crisjr@pm.me>
2023-02-16 18:52:58 -0300

    Check if file decompress fails gracefully

--- test/br/bsb/liberdade/baas/tar/decompress_test.clj
+++ test/br/bsb/liberdade/baas/tar/decompress_test.clj
@@ -3,6 +3,7 @@
             [br.bsb.liberdade.baas.tar.decompress :as untar]))
 
 (def good-test-file "./resources/actions.tar.gz")
+(def sad-test-file "./resources/pokemon.jpg")
 
 (deftest untar-file--happy-cases
   (testing "It's possible to decompress a tar file and not leave anything behind"
@@ -14,4 +15,16 @@
       (is (some? result))
       (is (= 2 (count result)))
       (is (= temp-files-before temp-files-after)))))
+
+(deftest untar-file--sad-cases
+  (testing "Decompressing invalid tar files fails gracefully"
+    (let [raw-binary (untar/slurp-bytes sad-test-file)
+          temp-files-before (untar/list-files "/tmp")
+	  invalid-file-result (untar/extract raw-binary)
+	  nil-result (untar/extract nil)
+	  temp-files-after (untar/list-files "/tmp")]
+      (is (some? raw-binary))
+      (is (= temp-files-before temp-files-after))
+      (is (nil? invalid-file-result))
+      (is (nil? nil-result)))))
 
