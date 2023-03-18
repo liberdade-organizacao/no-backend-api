df0fc13037ff6e5243fb330315a1464b14b9c10a
Cris Jr <crisjr@pm.me>
2023-03-18 11:11:02 -0300

    slowly getting there

--- scripts/file_size.fs
+++ scripts/file_size.fs
@@ -1,31 +1,111 @@
 32 constant key_length
+4 constant eot
 variable rec_key
+variable reached?
 
 : read_key
 	0
 	begin
 		dup
-		key dup rot rec_key cells + !
+		key dup rot cells rec_key + !
 		swap 1 + dup rot
 	58 = swap key_length >= or until
 	drop
 ;
 
 : debug_key
-	0
+	key_length 0 do
+		rec_key i cells + @ emit
+	loop
+;
+
+: is_rec_key_file_size?
+	0 cells rec_key + @ 102 =
+	1 cells rec_key + @ 105 =
+	2 cells rec_key + @ 108 =
+	3 cells rec_key + @ 101 =
+	4 cells rec_key + @ 95 =
+	5 cells rec_key + @ 115 =
+	6 cells rec_key + @ 105 =
+	7 cells rec_key + @ 122 =
+	8 cells rec_key + @ 101 =
+	and and and and and and and and
+;
+
+: is_rec_key_contents?
+	0 cells rec_key + @ 99 =
+	1 cells rec_key + @ 111 =
+	2 cells rec_key + @ 110 =
+	3 cells rec_key + @ 116 =
+	4 cells rec_key + @ 101 =
+	5 cells rec_key + @ 115 =
+	6 cells rec_key + @ 110 =
+	7 cells rec_key + @ 116 =
+	8 cells rec_key + @ 115 =
+	and and and and and and and and
+;
+
+: skip_line begin key 13 = until ;
+
+: write_key
+	key_length 0 do
+		rec_key i cells + @ emit
+	loop
+;
+
+: write_value
 	begin
-		dup
-		rec_key cells @ emit
-		1 + dup
-	32 >= until
-	drop
+		key dup
+		emit
+	13 = until
+;
+
+: end_reached?
+	false reached? !
+	key_length 0 do
+		rec_key i cells + @
+		eot =
+		reached? @ or
+		reached? !
+	loop
+	reached? @
 ;
 
 ( ######## )
 ( # MAIN # )
 ( ######## )
-( initializing variables )
-rec_key key_length cells allot
-read_key
-debug_key
+: setup
+	rec_key key_length cells allot
+	key_length 0 do
+		0 rec_key i cells + !
+	loop
+
+	false reached? !
+;
+
+: draw
+	read_key
+	debug_key
+	is_rec_key_file_size? if
+		skip_line
+	else is_rec_key_contents? if
+		( TODO write rec key )
+		( TODO count contents length )
+		( write_key )
+		write_value
+	else
+		( TODO write key )
+		( TODO write value )
+		( write_key )
+		write_value
+	then then
+;
+
+: main
+	setup
+	begin draw
+	end_reached? until
+;
+
+main
 
