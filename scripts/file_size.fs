742537b4d8ccd4c4965464b814362a63ed20fd74
Cris Jr <crisjr@pm.me>
2023-03-28 16:30:05 -0300

    Why is EOT being written in the rec vector?

--- scripts/file_size.fs
+++ scripts/file_size.fs
@@ -1,7 +1,14 @@
 32 constant key_length
-4 constant eot
+4 constant ascii_eot
+10 constant line_feed
+58 constant ascii_colon
 variable rec_key
 variable reached?
+variable temp
+
+( memory should be allocated before running everything! )
+rec_key key_length cells allot
+false reached? !
 
 : read_key
 	0
@@ -9,62 +16,66 @@ variable reached?
 		dup
 		key dup rot cells rec_key + !
 		swap 1 + dup rot
-	58 = swap key_length >= or until
+	ascii_colon = swap key_length >= or until
 	drop
 ;
 
 : debug_key
 	key_length 0 do
-		rec_key i cells + @ emit
+		rec_key i cells + @ .
 	loop
 ;
 
 : is_rec_key_file_size?
-	0 cells rec_key + @ 102 =
-	1 cells rec_key + @ 105 =
-	2 cells rec_key + @ 108 =
-	3 cells rec_key + @ 101 =
-	4 cells rec_key + @ 95 =
-	5 cells rec_key + @ 115 =
-	6 cells rec_key + @ 105 =
-	7 cells rec_key + @ 122 =
-	8 cells rec_key + @ 101 =
+	0 cells rec_key + @ 102 = \ f
+	1 cells rec_key + @ 105 = \ i
+	2 cells rec_key + @ 108 = \ l
+	3 cells rec_key + @ 101 = \ e
+	4 cells rec_key + @ 95 =  \ _
+	5 cells rec_key + @ 115 = \ s
+	6 cells rec_key + @ 105 = \ i
+	7 cells rec_key + @ 122 = \ z
+	8 cells rec_key + @ 101 = \ e
 	and and and and and and and and
 ;
 
 : is_rec_key_contents?
-	0 cells rec_key + @ 99 =
-	1 cells rec_key + @ 111 =
-	2 cells rec_key + @ 110 =
-	3 cells rec_key + @ 116 =
-	4 cells rec_key + @ 101 =
-	5 cells rec_key + @ 115 =
-	6 cells rec_key + @ 110 =
-	7 cells rec_key + @ 116 =
-	8 cells rec_key + @ 115 =
+	0 cells rec_key + @ 99 =  \ c
+	1 cells rec_key + @ 111 = \ o
+	2 cells rec_key + @ 110 = \ n
+	3 cells rec_key + @ 116 = \ t
+	4 cells rec_key + @ 105 = \ e
+	5 cells rec_key + @ 110 = \ n
+	6 cells rec_key + @ 116 = \ t
+	7 cells rec_key + @ 115 = \ s
 	and and and and and and and and
 ;
 
-: skip_line begin key 13 = until ;
+: skip_line 
+	begin key line_feed = until 
+;
 
 : write_key
-	key_length 0 do
-		rec_key i cells + @ emit
-	loop
+	-1 temp !
+	begin
+		1 temp +!
+		temp @ cells rec_key + @
+		dup emit
+	ascii_colon = until
 ;
 
 : write_value
 	begin
 		key dup
 		emit
-	13 = until
+	line_feed = until
 ;
 
 : end_reached?
 	false reached? !
 	key_length 0 do
 		rec_key i cells + @
-		eot =
+		ascii_eot =
 		reached? @ or
 		reached? !
 	loop
@@ -75,35 +86,34 @@ variable reached?
 ( # MAIN # )
 ( ######## )
 : setup
-	rec_key key_length cells allot
 	key_length 0 do
 		0 rec_key i cells + !
 	loop
-
-	false reached? !
 ;
 
 : draw
+	setup
 	read_key
-	debug_key
 	is_rec_key_file_size? if
 		skip_line
 	else is_rec_key_contents? if
 		( TODO write rec key )
 		( TODO count contents length )
 		( write_key )
-		write_value
+		\ write_value
+		skip_line
 	else
 		( TODO write key )
 		( TODO write value )
-		( write_key )
+		write_key
 		write_value
 	then then
 ;
 
 : main
 	setup
-	begin draw
+	begin
+		draw
 	end_reached? until
 ;
 
