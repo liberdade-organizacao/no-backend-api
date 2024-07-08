21bb0cc6f853b8c66e56af2c32f4d97034602410
Cris Silva Jr <crisjr@pm.me>
2024-07-08 19:20:27 +0100

    manage local files

---
+++ src/br/bsb/liberdade/baas/fs.clj
@@ -0,0 +1,24 @@
+(ns br.bsb.liberdade.baas.fs
+  (:require [clojure.java.io :as io]))
+
+(def fs-folder "./db/fs")
+
+(defn- filepath [filename]
+  (str fs-folder "/" filename))
+
+(defn write-file [filename contents]
+  (io/make-parents filename)
+  (spit (filepath filename) contents))
+
+(defn read-file [filename]
+  (try
+    (slurp (filepath filename))
+    (catch Exception ex
+      nil)))
+
+(defn delete-file [filename]
+  (try
+    (io/delete-file (filepath filename))
+    (catch Exception ex
+      nil)))
+
