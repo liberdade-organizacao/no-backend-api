00b75d451f0b4132fefa7694b5c85f4105b18729
Cris Silva Jr. <crisjr@pm.me>
2024-12-16 15:37:09 +0000

    Version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject br.bsb.liberdade.baas.api "0.2.0"
+(defproject br.bsb.liberdade.baas.api "0.3.0"
   :description "Backend as a Service API"
   :url "https://www.liberdade.bsb.br/"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
