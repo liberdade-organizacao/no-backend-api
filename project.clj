aeef4fc711f4fd7671e99f1ceabfbec700a4f874
Cris Silva Jr. <crisjr@pm.me>
2026-07-18 20:45:23 +0100

    upgrade out of date packages

--- project.clj
+++ project.clj
@@ -3,20 +3,20 @@
   :url "https://www.liberdade.bsb.br/"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
-                 [compojure "1.7.1"]
-                 [http-kit "2.8.0"]
-                 [ring/ring-defaults "0.5.0"]
-                 [org.clojure/data.json "2.5.0"]
-                 [selmer "1.12.61"]
+  :dependencies [[org.clojure/clojure "1.12.5"]
+                 [compojure "1.7.2"]
+                 [http-kit "2.8.1"]
+                 [ring/ring-defaults "0.7.1"]
+                 [org.clojure/data.json "2.5.2"]
+                 [selmer "1.13.4"]
                  [buddy/buddy-sign "3.6.1-359"]
                  [org.clojars.liberdade/strint "0.0.1"]
                  [jumblerg/ring-cors "3.0.0"]
-                 [com.github.seancorfield/next.jdbc "1.3.939"]
-                 [org.xerial/sqlite-jdbc "3.46.1.0"]
+                 [com.github.seancorfield/next.jdbc "1.3.1118"]
+                 [org.xerial/sqlite-jdbc "3.53.2.0"]
                  [middlesphere/clj-compress "0.1.0"]
                  [clojure-msgpack "1.2.1"]
-                 [clj-http "3.13.0"]]
+                 [clj-http "3.13.1"]]
   :min-lein-version "2.9.8"
   :main ^:skip-aot br.bsb.liberdade.baas.api
   :target-path "target/%s"
